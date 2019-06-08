4a95f66a53018f837117b9ae7f3a25f503eda298
root <root@GARY1Q2-laptop.localdomain>
2019-06-08 20:36:30 +1000

    Taxed code from stickerboard to make it runnable
    Users can join server with usernames and then disconnect. Nothing is really drawn on the screen yet cept for some HUD
    
    Bugs:
    1. PlayerNum can get to 0 if player still alive when server was down and they leave
    
    Issues:
    1. Canvas size too big, also wtf is a canvas thing in game.js for?

--- server.js
+++ server.js
@@ -3,26 +3,56 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var socketIO = require('socket.io');
+
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 5000);
+
+
+app.set('port', (process.env.PORT || 6969));
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+    response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+// Starts the server
+server.listen(app.get('port'), function() {
+  console.log(`Starting server on port ${app.get('port')}`);
 });
 
-// Add the WebSocket handlers
+// Variables
+playerNum = 0;
+
+// Shut server down gracefully upon CTRL + C or interrupt
+process.on('SIGINT', function() {
+	console.log("I'm shutting down baiii");
+	io.sockets.emit('shutdown');
+	process.exit()
+});
+
+// Handles new player connecitons
 io.on('connection', function(socket) {
-});
 
-setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
+	// New player submitted their name
+	socket.on('new player', function(data) {
+		if (data) {
+			playerNum++;
+	    	console.log("[" + data + "] has joined with id [" + socket.id + "] ------------- total players online = " + playerNum);
+			io.sockets.emit('playerNum', playerNum);   // Update players on player count
+
+		// Error if null username submitted
+		} else {
+			console.log("no name submitted");
+			socket.emit('nullName');
+		}
+	});
+
+	// Disconnect player and update players on new player count
+	socket.on('disconnect', function() {
+		playerNum--;
+		console.log("player with id [" + socket.id + "] DISCONNECTED!!! ---------- players remaining = " + playerNum);
+		io.sockets.emit('playerNum', playerNum);
+	});
+});
--- static/game.js
+++ static/game.js
@@ -1,4 +1,90 @@
 var socket = io();
-socket.on('message', function(data) {
-  console.log(data);
+
+// Local variables
+var playerName = "";
+var shutdown = false;
+
+
+document.getElementById('userInput').focus();   // Focus the username field
+
+/*
+===========================================================================
+// Initialising the canvas variable
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+canvas.width = 3000;
+canvas.height = 2000;
+console.log("canvas width = " + document.getElementById('hud').value + "---- canvas height = " + canvas.style.height);
+context.font = "10px Arial";
+===========================================================================
+*/
+
+// Player number updated
+socket.on('playerNum', function(data) {
+
+	// Remove login form if it's still there
+	if (document.getElementById('login').style.display != "none") {
+		document.getElementById('login').style.display = "none";
+		document.getElementById('hud').style.display = "block";
+		document.getElementById('playerNum').style.display = "block";
+		document.getElementById('playerName').style.display = "block";
+	}
+
+    document.getElementById('playerNum').innerHTML = "Players online: " + data;
+    console.log("got the num packet =" + data);
 });
+
+
+// User didn't enter a name
+socket.on('nullName', function() {
+	var toChange = document.getElementById('login').childNodes[0];
+	toChange.nodeValue = 'Enter your name: User must not be null';
+    document.getElementById('userInput').focus();
+});
+
+// Server shutting down, prevent new actions
+socket.on('shutdown', function() {
+	shutdown = true;
+
+	/*
+	shutdown = true (should negate anymore actions that the player makes)
+	*/
+});
+
+// Set username from login prompt
+function setName() {
+    playerName = document.getElementById('userInput').value;
+    console.log("user's name = " + playerName);
+
+    document.getElementById('playerName').innerHTML = playerName;
+
+    // Notify server to setup new player
+	socket.emit('new player', playerName);
+}
+
+// Submit username via enter key
+document.getElementById('userInput').onkeypress = function(event) {
+	console.log("enter pressed yee the boiz");
+    switch (event.keyCode) {
+    	case 13:
+    	    setName();
+    	    break;
+    }
+};
+
+// Disconnect from the server GRACEFULLY
+function disconnect() {
+	console.log("Requesting to disconnect...");
+	socket.disconnect();
+}
+
+
+/*
+// Update the whole canvas locally
+function updateCanvas() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawCoords();
+	drawWords();
+	drawMouse();
+}
+*/
