527e765fd102688848e85aa860ff7d6ded0dfca7
Gary1q2 <gary1q2@hotmail.com>
2019-09-27 17:20:38 +1000

    32 server tick + smooth movement implemented
    -Server runs on 32 tick intervals
    -Client still uses requestAF to render, but has to adjust the playerSpeed (becus server & client run on different speeds)
    -Cleaned up alot of code and added in more helper functions eg.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var port = 6969;
-var ip = "0.0.0.0";
+const port = 6969;
+const ip = "0.0.0.0";
 
 // Dependencies
 var express = require('express');
@@ -26,10 +26,17 @@ server.listen(app.get('port'), ip, function() {
   console.log(`Starting server on port ${app.get('port')}`);
 });
 
-// Variables
-var playerNum = 0;
-var playerList = {};
-var chatLog = [];
+// Variables   
+var pList = {};  // Stores player information
+var socketList = {};  // Stores player sockets
+var chatLog = [];     // Stores player messages
+
+var playerNum = 0; 
+const playerSpeed = 50; // Pixels moved per gametick
+const gameTick = 5;
+
+
+
 
 // Shut server down gracefully upon CTRL + C or interrupt
 process.on('SIGINT', function() {
@@ -51,13 +58,18 @@ io.on('connection', function(socket) {
 				name: data,
 				xPos: 100,
 				yPos: 100,
+				xDes: -1,
+				yDes: -1,
 				facing: "right"
 			};
 
-			// Add new player into the server information and give player the server state
-			playerList[socket.id] = playerInfo;
-			socket.emit('initDone');
-			socket.emit('gameState', playerList);
+			// Add new player into the server information + save socket
+			socketList[socket.id] = socket;
+			pList[socket.id] = playerInfo;
+
+			// Give player the server state + specific values
+			socket.emit('initDone', {spd: playerSpeed, tick: gameTick});
+			socket.emit('gameState', pList);
 			console.log(data);	
 
 			// Tell other players to update their local list about new player
@@ -78,37 +90,38 @@ io.on('connection', function(socket) {
 
 	// Disconnect player and update players on new player count
 	socket.on('disconnect', function() {
-		if (playerList[socket.id] != undefined) {
+		if (pList[socket.id] != undefined) {
 			playerNum--;
 			console.log("player with id [" + socket.id + "] DISCONNECTED!!! ---------- players remaining = " + playerNum);
 			io.sockets.emit('playerNum', playerNum);
 			socket.broadcast.emit('otherLeave', socket.id);
-			delete playerList[socket.id];
+			delete pList[socket.id];
 		}
 	});
 
 	// Player selected a spot to move to
 	socket.on('movement', function(data) {
-		if (playerList[socket.id] != undefined) {
+		if (pList[socket.id] != undefined) {
 			console.log(data);
 			console.log("socket id = " + socket.id);
 
 			//=========================================================
 			// Check if location is valid <- do once you implement location
 			// CHANGE ..... its sending its current position
+			pList[socket.id].xDes = data.xDes;
+			pList[socket.id].yDes = data.yDes;
 
-			playerList[socket.id].xPos = data.x;
-			playerList[socket.id].yPos = data.y;
-			playerList[socket.id].facing = data.facing;
-			console.log(playerList);
-
-			socket.broadcast.emit('updateState', playerList);
+			if (pList[socket.id].xDes - pList[socket.id].xPos >= 0) {
+				pList[socket.id].facing = "right";
+			} else {
+				pList[socket.id].facing = "left";
+			}
 		}
 	});
 
 	// Player sent a message
 	socket.on('chat', function(data) {
-		if (playerList[socket.id] != undefined) {
+		if (pList[socket.id] != undefined) {
 			console.log("Received message[" + data + "] from player " + socket.id);
 			socket.broadcast.emit('playerChat', {
 				id: socket.id,
@@ -121,3 +134,104 @@ io.on('connection', function(socket) {
 	});
 
 });
+
+
+
+// Server game loop @ 32 ticks
+setInterval(function() {
+	updatePlayerMovement();
+	sendPlayerMovement();
+
+}, 1000/gameTick);
+
+
+
+// Broadcast new player positions to all clients
+function sendPlayerMovement() {
+
+	// Create array containing only player positions + info
+	var playerLoc = {};
+	for (var player in pList) {
+		var pos = {
+			xPos: pList[player].xPos,
+			yPos: pList[player].yPos,
+			facing: pList[player].facing
+		}
+		playerLoc[player] = pos;
+	}
+
+	// Broadcast everyone's new positions
+	for (var player in socketList) {
+		socketList[player].emit('updateState', playerLoc);
+	}
+}
+
+// Update all player movement
+function updatePlayerMovement() {
+
+	var speed = playerSpeed;
+
+	for (var i in pList) {
+		if (pList[i].xDes != -1 && pList[i].yDes != -1) {
+			//console.log("xDes =" + pList[i].xDes + "    yDes = " + pList[i].yDes);
+			var moveAngle = getMoveAngle(pList[i].xPos, pList[i].xDes, pList[i].yPos, pList[i].yDes);
+			pList[i].xPos = calculateXPos(pList[i].xPos, pList[i].xDes, speed, moveAngle);
+			pList[i].yPos = calculateYPos(pList[i].yPos, pList[i].yDes, speed, moveAngle);
+
+			// Player reached destination, no more moving
+			if (pList[i].xPos == pList[i].xDes && pList[i].yPos == pList[i].yDes) {
+				pList[i].xDes = -1;
+				pList[i].yDes = -1;
+				console.log("stopped moving");
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Moves the player x location on the x axis
+// Given the player's current x position, destination x postion & speed
+function calculateXPos(xPos, xDes, speed, angle) {
+	if (Math.abs(xDes - xPos) > speed*Math.cos(angle)) {
+		if (xPos < xDes) {
+			return (xPos + (speed*Math.cos(angle)));
+		} else if (xPos > xDes) {
+			return (xPos - (speed*Math.cos(angle)));
+		}
+	} else if (Math.abs(xDes - xPos) > 0) {
+		return xDes;
+	} else {
+		return xPos;
+	}
+}
+
+// Moves the player y location on the y axis
+// Given the player's current y position, destination y postion & speed
+function calculateYPos(yPos, yDes, speed, angle) {
+	if (Math.abs(yDes - yPos) > speed*Math.sin(angle)) {
+		if (yPos < yDes) {
+			return (yPos + (speed*Math.sin(angle)));
+		} else if (yPos > yDes) {
+			return (yPos - (speed*Math.sin(angle)));
+		}
+	} else if (Math.abs(yDes - yPos) > 0) {
+		return yDes;
+	} else {
+		return yPos;
+	}
+}
+
+// Calculate the move angle given the player's current position and destination
+function getMoveAngle(xPos, xDes, yPos, yDes) {
+	return Math.abs(Math.atan((Math.abs(yDes-yPos)/(xDes-xPos))));
+}
--- static/game.js
+++ static/game.js
@@ -9,24 +9,23 @@ var player = {
 	name: "",
 	xPos: 100,
 	yPos: 100,
-	xPosDes: 100,
-	yPosDes: 100,
+	xDes: 100,
+	yDes: 100,
 	moveAngle: 0,
 	moving: false,
 	facing: "right",
 	chat: ""
 };
 
-// Local playerList array
-var localPlayerList;               // Local array containing other player's position
-var otherMove = [];                // Array containing queue of other player's positions to be drawn
-var currMove;                      // Pop localPlayerList here to interpolate position
-var interpolate = false;           
 
+var localPList;               // Local array containing other player's position
 
 var displayChat = 0;               // Display local player's chat?
 var otherChat = {};                // Store's other player's chats to be rendered
 
+
+var playerSpeed;
+var gameTick;
 
 // Global stick figure image
 var stickman = new Image();
@@ -74,7 +73,10 @@ socket.on('nullName', function() {
 });
 
 // Let player know we have been initialised properly
-socket.on('initDone', function() { 
+socket.on('initDone', function(data) { 
+
+	playerSpeed = data.spd;
+	gameTick = data.tick;
 
 	// Remove login form
 	document.getElementById('login').style.display = "none";
@@ -117,32 +119,33 @@ function disconnect() {
 
 // Receive initial gamestate from server
 socket.on('gameState', function(data) {
-	localPlayerList = data;
+	localPList = data;
 });
 
 
 // Another player joined, so update your array
 socket.on('otherJoin', function(data, id) {
 	if (initialised) {
-		localPlayerList[id] = data;
+		localPList[id] = data;
 	}
 });
 
 // A player left, so remove them from your array
 socket.on('otherLeave', function(id) {
 	if (initialised) {
-		delete localPlayerList[id];
+		delete localPList[id];
 	}
 });
-
 
 
 // Receive update on player's locations
 socket.on('updateState', function(data) {
 
-    // Push other player's location onto queue to be rendered
-	otherMove.push(data);
-	console.log("Someone is moving... [" + otherMove.length + "]");
+	for (i in data) {
+		localPList[i].xDes = data[i].xPos;
+		localPList[i].yDes = data[i].yPos;
+		localPList[i].facing = data[i].facing;
+	}
 });
 
 // Receive chat from other player
@@ -178,26 +181,30 @@ document.addEventListener("click", function(event) {
 		// Should client side check here - once you implement locations
 		//===========================================================
 
-		player.xPosDes = event.offsetX;
-		player.yPosDes = event.offsetY;
-		player.moveAngle = getMoveAngle(player.xPos, player.xPosDes, player.yPos, player.yPosDes);
-		console.log("I want to move to [" + player.xPosDes + " , " + player.yPosDes + "]   Angle = " 
+		player.xDes = event.offsetX;
+		player.yDes = event.offsetY;
+		player.moveAngle = getMoveAngle(player.xPos, player.xDes, player.yPos, player.yDes);
+		console.log("I want to move to [" + player.xDes + " , " + player.yDes + "]   Angle = " 
 			                                    + Math.round(player.moveAngle*180/Math.PI) + " degs");
 		player.moving = true;
 
 		// Set the facing direction
-		if (player.xPosDes - player.xPos >= 0) {
+		if (player.xDes - player.xPos >= 0) {
 			player.facing = "right";
 			console.log("right");
 		} else {
 			console.log("left");
 			player.facing = "left";
 		}
-
 		console.log("player facing = " + player.facing);
 
-		// Focus the chat box
 		document.getElementById('chatbox').focus();
+
+		var target = {
+			xDes: player.xDes,
+			yDes: player.yDes
+		}
+		socket.emit('movement', target);
 	}
 });
 
@@ -229,43 +236,114 @@ document.onkeypress = function(event) {
 };
 
 
-// Send player position to server at every game tick
-setInterval(function() {
-
-	if (initialised) {
-
-		// Send the current position of player to the server if it is UPDATING
-		if (player.xPos != player.xPosDes || player.yPos != player.yPosDes) {
-			var coords = {
-				x: player.xPos,
-				y: player.yPos,
-				facing: player.facing
-			};
-			//console.log(coords);
-			socket.emit('movement', coords);
-
-			console.log("sent my movement to server...");
-			console.log(coords);
-		}
-	}
-}, 1000);
-
 
 
 // Client game loop
 function mainLoop() {
 	if (initialised) {
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		updatePlayerLoc();
+
+		// Draw player position
+		updatePlayer();
+		updateOtherPlayers();
 		drawPlayer();
-		updateDrawOtherPlayers();
+		drawOtherPlayers();
+		
+		// Draw chat
 		drawPlayerChat();
 		drawOtherChat();
+
 	}
 	requestAnimationFrame(mainLoop);
 }
 
 requestAnimationFrame(mainLoop);
+
+
+
+
+// Update the current player's location
+function updatePlayer() {
+
+	// Moves the player from current position to destination position (client side prediction)
+	var speed = (gameTick*playerSpeed)/60;
+	player.xPos = calculateXPos(player.xPos, player.xDes, speed, player.moveAngle);
+	player.yPos = calculateYPos(player.yPos, player.yDes, speed, player.moveAngle);
+}
+
+
+// Draw the current player
+function drawPlayer() {
+	if (player.facing == "right") {
+		context.drawImage(stickman, Math.round(player.xPos-stickman.width/2), Math.round(player.yPos-stickman.height/2));	
+	} else {
+		context.translate(Math.round(player.xPos+stickman.width/2), Math.round(player.yPos-stickman.height/2))
+		context.scale(-1, 1);
+		context.drawImage(stickman, 0, 0);
+		context.setTransform(1, 0, 0, 1, 0, 0)
+	}
+
+	// Debugging variables
+	drawStickVar(player.name, player.xPos, player.yPos + 40);
+	drawStickVar("xPos: " + Math.round(player.xPos), player.xPos, player.yPos + 70);
+	drawStickVar("yPos: " + Math.round(player.yPos), player.xPos, player.yPos + 90);
+	drawStickVar("facing: " + player.facing, player.xPos, player.yPos + 110);
+}
+
+
+
+// Update other players
+function updateOtherPlayers() {
+
+	var speed = (gameTick*playerSpeed)/60;
+
+	// Update the positions of other players (if they requiring moving that is)
+	for (i in localPList) {
+		if (i != socket.id) {
+			if (localPList[i].xDes != -1 && localPList[i].yDes != -1) {
+				var moveAngle = getMoveAngle(localPList[i].xPos, localPList[i].xDes, localPList[i].yPos, localPList[i].yDes);
+				localPList[i].xPos = calculateXPos(localPList[i].xPos, localPList[i].xDes, speed, moveAngle);
+				localPList[i].yPos = calculateYPos(localPList[i].yPos, localPList[i].yDes, speed, moveAngle);
+
+				// Player reached destination, no more moving
+				if (localPList[i].xPos == localPList[i].xDes && localPList[i].yPos == localPList[i].yDes) {
+					localPList[i].xDes = -1;
+					localPList[i].yDes = -1;
+				}
+			}
+		}
+	}
+
+}
+
+// Draw other players
+function drawOtherPlayers() {
+
+	// Draw the positions of other players
+	for (i in localPList) {
+		if (i != socket.id) {
+
+			// Draw stickman facing left or right
+			if (localPList[i].facing == "right") {
+				context.drawImage(stickman, Math.round(localPList[i].xPos-stickman.width/2), Math.round(localPList[i].yPos-stickman.height/2));	
+			} else {
+				context.translate(Math.round(localPList[i].xPos+stickman.width/2), Math.round(localPList[i].yPos-stickman.height/2));
+				context.scale(-1, 1);
+				context.drawImage(stickman, 0, 0);
+				context.setTransform(1, 0, 0, 1, 0, 0)
+			}
+
+			// Debugging variables
+			drawStickVar(localPList[i].name, localPList[i].xPos, localPList[i].yPos + 40);
+			drawStickVar("xPos: " + Math.round(localPList[i].xPos), localPList[i].xPos, localPList[i].yPos + 70);
+			drawStickVar("yPos: " + Math.round(localPList[i].yPos), localPList[i].xPos, localPList[i].yPos + 90);
+			drawStickVar("facing: " + localPList[i].facing, localPList[i].xPos, localPList[i].yPos + 110);
+		}
+	}
+}
+
+
+
 
 
 // Draw the player's chat on top of their own head
@@ -280,155 +358,26 @@ function drawPlayerChat() {
 function drawOtherChat() {
 	for (key in otherChat) {
 		if (otherChat[key].displayChat != 0) {
-			console.log("key = " + key + " xPos = " + Math.round(localPlayerList[key].xPos) + "yPos = " + Math.round(localPlayerList[key].yPos));
-			context.fillText(otherChat[key].chat, Math.round(localPlayerList[key].xPos-stickman.width/2), Math.round(localPlayerList[key].yPos-stickman.height/2-30));
+			console.log("key = " + key + " xPos = " + Math.round(localPList[key].xPos) + "yPos = " + Math.round(localPList[key].yPos));
+			context.fillText(otherChat[key].chat, Math.round(localPList[key].xPos-stickman.width/2), Math.round(localPList[key].yPos-stickman.height/2-30));
 		}
 	}
 }
 
-
-// Update the current player's location
-function updatePlayerLoc() {
-
-	// Moves the player from current position to destination position (client side prediction)
-	var speed = 8;
-	player.xPos = calculateXPos(player.xPos, player.xPosDes, speed, player.moveAngle);
-	player.yPos = calculateYPos(player.yPos, player.yPosDes, speed, player.moveAngle);
-}
-
-
-// Draw the player
-function drawPlayer() {
-	// Draws the current player
-	if (player.facing == "right") {
-		context.drawImage(stickman, Math.round(player.xPos-stickman.width/2), Math.round(player.yPos-stickman.height/2));	
-	} else {
-		context.translate(Math.round(player.xPos+stickman.width/2), Math.round(player.yPos-stickman.height/2))
-		context.scale(-1, 1);
-		context.drawImage(stickman, 0, 0);
-		context.setTransform(1, 0, 0, 1, 0, 0)
-	}
-
-	//console.log("pplayer facing = " + player.facing);	
-
-	context.fillText(player.name, Math.round(player.xPos-stickman.width/2), Math.round(player.yPos+stickman.height/2+40));
-	context.fillText("xPos: " + Math.round(player.xPos), Math.round(player.xPos-stickman.width/2), Math.round(player.yPos+stickman.height/2+70));
-	context.fillText("yPos: " + Math.round(player.yPos), Math.round(player.xPos-stickman.width/2), Math.round(player.yPos+stickman.height/2+90));
-	context.fillText("facing: " + player.facing, Math.round(player.xPos-stickman.width/2), Math.round(player.yPos+stickman.height/2+110));
-}
-
-
-
-// Update and draw other players
-function updateDrawOtherPlayers() {
-
-	// Pop the player's movement off the queue and RENDER THEM ALL!!!
-	//===================================================================
-
-	//==================================================================
-
-	// Other players have moved, interpolate their positions
-	if (interpolate == false && otherMove.length > 0) {
-		currMove = otherMove.pop();
-		console.log("Popped off ["+ otherMove.length +"]")
-		interpolate = true;
-
-
-		// Update the other player's facing direction
-		for (key in currMove) {
-			localPlayerList[key].facing = currMove[key].facing;
-		}
-	} 
-
-	if (interpolate == true) {
-		var done = true;
-		for (i in currMove) {
-			if (i != socket.id) {
-				console.log("BEFOREEEE  LocalList = ["+localPlayerList[i].xPos+","+localPlayerList[i].yPos+"]    CurrMove = ["+currMove[i].xPos+","+currMove[i].yPos+"]");
-				
-				otherMoveAngle = getMoveAngle(localPlayerList[i].xPos, currMove[i].xPos, localPlayerList[i].yPos, currMove[i].yPos);
-				console.log("other move angle = " + otherMoveAngle + "   cos(moveANgle) = " + Math.cos(otherMoveAngle));
-
-				localPlayerList[i].xPos = calculateXPos(localPlayerList[i].xPos, currMove[i].xPos, 8, otherMoveAngle);
-				localPlayerList[i].yPos = calculateYPos(localPlayerList[i].yPos, currMove[i].yPos, 8, otherMoveAngle);
-
-				// Draw stickman facing left or right
-				if (localPlayerList[i].facing == "right") {
-					context.drawImage(stickman, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos-stickman.height/2));	
-				} else {
-					context.translate(Math.round(localPlayerList[i].xPos+stickman.width/2), Math.round(localPlayerList[i].yPos-stickman.height/2))
-					context.scale(-1, 1);
-					context.drawImage(stickman, 0, 0);
-					context.setTransform(1, 0, 0, 1, 0, 0)
-				}
-
-
-				// Draw helpful variables
-				context.fillText(localPlayerList[i].name, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+40));
-				context.fillText("xPos: " + localPlayerList[i].xPos, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+70));
-				context.fillText("yPos: " + localPlayerList[i].yPos, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+90));
-				context.fillText("facing: " + currMove[i].facing, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+110));
-			
-
-				if (localPlayerList[i].xPos != currMove[i].xPos || localPlayerList[i].yPos != currMove[i].yPos) {
-					done = false;
-					console.log("done set to false");
-					console.log(localPlayerList[i].xPos + " " + currMove[i].xPos + " " + localPlayerList[i].yPos + " " + currMove[i].yPos);
-				}
-				console.log("AFTER   LocalList = ["+localPlayerList[i].xPos+","+localPlayerList[i].yPos+"]    CurrMove = ["+currMove[i].xPos+","+currMove[i].yPos+"]");
-			}
-
-		}
-
-		if (done == true) {
-			interpolate = false;
-			console.log("interpolate FALSED");
-		}
-	
-
-
-
-
-
-	// Other players haven't moved, so just render the same location
-	} else {
-		//console.log("shud be printing local state");
-		for (i in localPlayerList) {
-			if (i != socket.id) {
-
-				// Draw stickman facing left or right
-				if (localPlayerList[i].facing == "right") {
-					context.drawImage(stickman, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos-stickman.height/2));	
-				} else {
-					context.translate(Math.round(localPlayerList[i].xPos+stickman.width/2), Math.round(localPlayerList[i].yPos-stickman.height/2))
-					context.scale(-1, 1);
-					context.drawImage(stickman, 0, 0);
-					context.setTransform(1, 0, 0, 1, 0, 0)
-				}
-			
-				// Draw helpful variables
-				context.fillText(localPlayerList[i].name, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+40));
-				context.fillText("xPos: " + localPlayerList[i].xPos, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+70));
-				context.fillText("yPos: " + localPlayerList[i].yPos, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+90));
-				context.fillText("facing: " + localPlayerList[i].facing, Math.round(localPlayerList[i].xPos-stickman.width/2), Math.round(localPlayerList[i].yPos+stickman.height/2+110));
-			}
-		}
-	}
-}
 
 
 
 // Moves the player x location on the x axis
 // Given the player's current x position, destination x postion & speed
-function calculateXPos(xPos, xPosDes, speed, angle) {
-	if (Math.abs(xPosDes - xPos) > speed*Math.cos(angle)) {
-		if (xPos < xPosDes) {
+function calculateXPos(xPos, xDes, speed, angle) {
+	if (Math.abs(xDes - xPos) > speed*Math.cos(angle)) {
+		if (xPos < xDes) {
 			return (xPos + (speed*Math.cos(angle)));
-		} else if (xPos > xPosDes) {
+		} else if (xPos > xDes) {
 			return (xPos - (speed*Math.cos(angle)));
 		}
-	} else if (Math.abs(xPosDes - xPos) > 0) {
-		return xPosDes;
+	} else if (Math.abs(xDes - xPos) > 0) {
+		return xDes;
 	} else {
 		return xPos;
 	}
@@ -436,21 +385,27 @@ function calculateXPos(xPos, xPosDes, speed, angle) {
 
 // Moves the player y location on the y axis
 // Given the player's current y position, destination y postion & speed
-function calculateYPos(yPos, yPosDes, speed, angle) {
-	if (Math.abs(yPosDes - yPos) > speed*Math.sin(angle)) {
-		if (yPos < yPosDes) {
+function calculateYPos(yPos, yDes, speed, angle) {
+	if (Math.abs(yDes - yPos) > speed*Math.sin(angle)) {
+		if (yPos < yDes) {
 			return (yPos + (speed*Math.sin(angle)));
-		} else if (yPos > yPosDes) {
+		} else if (yPos > yDes) {
 			return (yPos - (speed*Math.sin(angle)));
 		}
-	} else if (Math.abs(yPosDes - yPos) > 0) {
-		return yPosDes;
+	} else if (Math.abs(yDes - yPos) > 0) {
+		return yDes;
 	} else {
 		return yPos;
 	}
 }
 
 // Calculate the move angle given the player's current position and destination
-function getMoveAngle(xPos, xPosDes, yPos, yPosDes) {
-	return Math.abs(Math.atan((Math.abs(yPosDes-yPos)/(xPosDes-xPos))));
+function getMoveAngle(xPos, xDes, yPos, yDes) {
+	return Math.abs(Math.atan((Math.abs(yDes-yPos)/(xDes-xPos))));
+}
+
+
+// Draws text starting from the origin of the given stickman position
+function drawStickVar(string, xPos, yPos) {
+	context.fillText(string, Math.round(xPos-stickman.width/2), Math.round(yPos+stickman.height/2));
 }
