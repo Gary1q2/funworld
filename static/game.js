7a5be935dfeb3fb0376dcb91bd9c15e6d901fbc7
Gary1q2 <gary1q2@hotmail.com>
2019-10-17 19:17:17 +1100

    Player can now move while inventory opened + added inventory button

--- static/game.js
+++ static/game.js
@@ -17,6 +17,10 @@ images.stickman = new Image();
 images.stickman.src = "static/stickman.png";
 images.bg = new Image();
 images.bg.src = "static/bg_test.png";
+images.inventClose = new Image();
+images.inventClose.src = "static/inventClose.png";
+images.inventOpen = new Image();
+images.inventOpen.src = "static/inventOpen.png";
 
 
 var socket = io();
@@ -216,72 +220,79 @@ socket.on("playerChat", function(data) {
 document.getElementById('canvas').addEventListener("click", function(event) {
 	if (initialised) {
 
-		// Clicked the inventory
-		if (inventOpen) {
+		// Clicked the inventory button on or off
+		if (event.offsetX >= 600-50 && event.offsetX <= 600+50 && event.offsetY >= 500-50 && event.offsetY <= 500+50) {
+			inventOpen = !inventOpen;	
+		} else {
 			var itemClicked = inventItemClicked(event.offsetX, event.offsetY);
 			debugMsg("CLICKED ON ITEM = " + itemClicked);
 
 			var equipClicked = equipItemClicked(event.offsetX, event.offsetY);
-			debugMsg("CLICKED on EQUIPMENT = " + equipClicked);
+			debugMsg("CLICKED on EQUIPMENT = " + equipClicked);		
 
-			// Assign the item clicked to player's equipped item
-			if (itemClicked != -1) {
-				if (items[itemClicked].equip == "head") {
-					player.head = itemClicked;
-				} else if (items[itemClicked].equip == "body") {
-					player.body = itemClicked;
-				} else if (items[itemClicked].equip == "hand") {
-					player.hand = itemClicked;
+			// Clicked the inventory or equipment
+			if (inventOpen && ((itemClicked != -1) || (equipClicked != -1))) {
+
+				// Assign the item clicked to player's equipped item
+				if (itemClicked != -1) {
+					if (items[itemClicked].equip == "head") {
+						player.head = itemClicked;
+					} else if (items[itemClicked].equip == "body") {
+						player.body = itemClicked;
+					} else if (items[itemClicked].equip == "hand") {
+						player.hand = itemClicked;
+					}
+
+					// Tell server equipped an item
+					socket.emit('equipItem', itemClicked);
+
+				} else if (equipClicked != -1) {
+					if (equipClicked == "head") {
+						player.head = -1;
+					} else if (equipClicked == "body") {
+						player.body = -1;
+					} else if (equipClicked == "hand") {
+						player.hand = -1;
+					}
+
+					// Tell server unequipped an item
+					socket.emit('unequipItem', equipClicked);
 				}
+				// ===============================================
+				// CAN ONLY CLICK ON EITHER INVENTORY OR EQUIPMENT ONLY RIGHT NOW
+				// OVERRIDESSSS
 
-				// Tell server equipped an item
-				socket.emit('equipItem', itemClicked);
-
-			} else if (equipClicked != -1) {
-				if (equipClicked == "head") {
-					player.head = -1;
-				} else if (equipClicked == "body") {
-					player.body = -1;
-				} else if (equipClicked == "hand") {
-					player.hand = -1;
-				}
-
-				// Tell server unequipped an item
-				socket.emit('unequipItem', equipClicked);
-			}
-			// ===============================================
-			// CAN ONLY CLICK ON EITHER INVENTORY OR EQUIPMENT ONLY RIGHT NOW
-			// OVERRIDESSSS
-
-			// ================================================
+				// ================================================
 
 
-		// Move the player
-		} else {
 
-			//===========================================================
-			// Should client side check here - once you implement locations
-			//===========================================================
-
-			player.xDes = event.offsetX;
-			player.yDes = event.offsetY;
-			mouse_x = event.offsetX;
-			mouse_y = event.offsetY;
-			playerMoveAngle = getMoveAngle(player.xPos, player.xDes, player.yPos, player.yDes);
-			debugMsg("I want to move to [" + player.xDes + " , " + player.yDes + "]   Angle = " 
-				                                    + Math.round(playerMoveAngle*180/Math.PI) + " degs");
-
-			// Set the facing direction
-			if (player.xDes - player.xPos >= 0) {
-				player.facing = "right";
-				debugMsg("right");
+			// Move the player	
 			} else {
-				debugMsg("left");
-				player.facing = "left";
-			}
-			debugMsg("player facing = " + player.facing);
 
-			document.getElementById('chatbox').focus();
+				//===========================================================
+				// Should client side check here - once you implement locations
+				//===========================================================
+
+				player.xDes = event.offsetX;
+				player.yDes = event.offsetY;
+				mouse_x = event.offsetX;
+				mouse_y = event.offsetY;
+				playerMoveAngle = getMoveAngle(player.xPos, player.xDes, player.yPos, player.yDes);
+				debugMsg("I want to move to [" + player.xDes + " , " + player.yDes + "]   Angle = " 
+					                                    + Math.round(playerMoveAngle*180/Math.PI) + " degs");
+
+				// Set the facing direction
+				if (player.xDes - player.xPos >= 0) {
+					player.facing = "right";
+					debugMsg("right");
+				} else {
+					debugMsg("left");
+					player.facing = "left";
+				}
+				debugMsg("player facing = " + player.facing);
+
+				document.getElementById('chatbox').focus();
+			}
 		}
 	}
 }, true);
@@ -353,7 +364,16 @@ function mainLoop() {
 		// Draw inventory (if opened)
 		if (inventOpen) {
 			drawInventory();
+			ctx.drawImage(images["inventClose"], 600-50, 500-50, 100, 100);
+		} else {
+			ctx.drawImage(images["inventClose"], 600-50, 500-50, 100, 100);
 		}
+
+		// Draw inventory on/off button rectangle
+		ctx.beginPath();
+		ctx.lineWidth = "2";
+		ctx.rect(600-50, 500-50, 100, 100);
+		ctx.stroke();
 
 		// Some debugging stuff
 		if (debug) { 
